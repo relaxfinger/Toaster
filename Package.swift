6e35e145be1f7ae0e2fad944739fe152cebd77ab
relaxfinger <relaxfinger@gmail.com>
2020-05-28 10:54:33 +0800

    modify dir

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Toaster",
-            dependencies: []
+            dependencies: [],
+            path: "Toaster"
         ),
         .testTarget(
             name: "ToasterTests",
